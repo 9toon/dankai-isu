8e663b312048d8dd12b4a5fbbbcb2016a8364f19
Yusuke Naito <yusuke-naito@cookpad.com>
2016-05-27 14:40:19 +0900

    Introduce redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # for redis
+  config.redis = {
+    host: 'localhost',
+    port: 6379
+  }
 end
