4b138e4d098f62fbefa793d4974bb1d90e36eb94
Hiroyuki Inoue <hiroyuki-inoue@cookpad.com>
2016-05-27 12:52:13 +0900

    Revert Recipe.where to Recipe.find

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -70,7 +70,7 @@ class RecipesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_recipe
-      @recipe = Recipe.where(id: params[:id]).first
+      @recipe = Recipe.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
